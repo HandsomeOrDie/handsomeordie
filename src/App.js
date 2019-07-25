b7dd76f5203b139395ce3f5f8daf784d2ebf90e4
frank.gu <frank.gu@quantinfotech.com>
2019-07-25 17:40:28 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Routes from "./Routes";
 import React, { Component } from "react";
 import { HashRouter } from "react-router-dom";
-import { createHashHistory } from "history";
+import history from "./history";
 
 class App extends Component {
     render() {
         return (
-            <HashRouter history={createHashHistory()}>
+            <HashRouter history={history}>
                 <div className="container">
                     <Routes type={this.props.type} />
                 </div>
--- src/home/index.js
+++ src/home/index.js
@@ -3,7 +3,9 @@ import React, { Component } from "react";
 class Home extends Component {
     render() {
         return (
-            <div>11111</div>
+            <div>
+                frankfrankfrankfrankfrankfrankfrankfrankfrankfrankfrankfrank
+            </div>
         );
     }
 }
