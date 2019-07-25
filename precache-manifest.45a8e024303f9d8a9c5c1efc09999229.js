630125886abbdb2b1c12b43ee4053fa7bfc2d181
frank.gu <frank.gu@quantinfotech.com>
2019-07-25 12:42:25 +0800

    Updates

---
+++ precache-manifest.45a8e024303f9d8a9c5c1efc09999229.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99d0c8379be1e66689fded75e2e8240",
+    "url": "/HandsomeOrDie/index.html"
+  },
+  {
+    "revision": "e345dbb323259060edf2",
+    "url": "/HandsomeOrDie/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/HandsomeOrDie/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "e345dbb323259060edf2",
+    "url": "/HandsomeOrDie/static/js/main.cdcdcf8b.chunk.js"
+  },
+  {
+    "revision": "beaf2f1cab74f115ac0d",
+    "url": "/HandsomeOrDie/static/js/runtime~main.14b487e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/HandsomeOrDie/static/media/logo.5d5d9eef.svg"
+  }
+]);
