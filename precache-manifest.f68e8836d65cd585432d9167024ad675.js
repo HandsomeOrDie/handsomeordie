0c1999c31886d4093c22bfdc6cee031ca6f19c04
frank.gu <frank.gu@quantinfotech.com>
2019-07-25 13:40:41 +0800

    Updates

---
+++ precache-manifest.f68e8836d65cd585432d9167024ad675.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65849181149bd8d7e7d0a0d38df76021",
+    "url": "/handsomeordie/index.html"
+  },
+  {
+    "revision": "63a1c74f67b9acd5c1a1",
+    "url": "/handsomeordie/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/handsomeordie/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "63a1c74f67b9acd5c1a1",
+    "url": "/handsomeordie/static/js/main.b42eb46f.chunk.js"
+  },
+  {
+    "revision": "5db1d90d2dde66770811",
+    "url": "/handsomeordie/static/js/runtime~main.edc600c1.js"
+  }
+]);
