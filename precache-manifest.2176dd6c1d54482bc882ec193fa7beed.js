720adc5b0c44e5aa59f31c0d73f27ae412c3ddb7
frank.gu <frank.gu@quantinfotech.com>
2019-07-25 19:25:21 +0800

    Updates

--- precache-manifest.2176dd6c1d54482bc882ec193fa7beed.js
+++ precache-manifest.2176dd6c1d54482bc882ec193fa7beed.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "038a6befdb311d90bcddbdd40b1dd1be",
+    "revision": "8be5263331e45fc5e06b9366b2eb440c",
     "url": "/handsomeordie/index.html"
   },
   {
-    "revision": "9e67f9514455f1f1fb17",
+    "revision": "76083b60f2d306c55dc4",
     "url": "/handsomeordie/static/css/main.34de6062.chunk.css"
   },
   {
-    "revision": "3c2f76062ec044d3a69b",
-    "url": "/handsomeordie/static/js/2.e71aba0c.chunk.js"
+    "revision": "07947b7d33a7e4abc8a3",
+    "url": "/handsomeordie/static/js/2.e6616b5c.chunk.js"
   },
   {
-    "revision": "9e67f9514455f1f1fb17",
-    "url": "/handsomeordie/static/js/main.bf5b8ccb.chunk.js"
+    "revision": "76083b60f2d306c55dc4",
+    "url": "/handsomeordie/static/js/main.80cef2f3.chunk.js"
   },
   {
     "revision": "5db1d90d2dde66770811",
